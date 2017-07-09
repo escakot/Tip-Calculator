e96b6dc5474d868b48c265a8b5e59d456d0b68e1
escakot <ec@neorennie.com>
2017-07-08 21:40:00 -0400

    Initial Commit

---
+++ Tip Calculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tip Calculator
+//
+//  Created by Errol Cheong on 2017-07-08.
+//  Copyright © 2017 Errol Cheong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
